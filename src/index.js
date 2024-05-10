e2690e075bb8addf087cc043ce516248837e848a
unknown <DoctorHays998@yandex.ru>
2024-05-10 21:55:46 +0300

    Added filters and adaptive disign

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 const addToDoForm = document.getElementById('form');
 const addToDoInput = document.getElementById('input');
-const toDoList = document.getElementById('toDoList');
+const toDoListContainer = document.getElementById('toDoList');
 const toggleAllToDoButton = document.getElementById('toggleAllToDo');
 const helperContainer = document.getElementById('helper');
 const toDoCount = document.getElementById('count');
 const clearCompletedToDo = document.getElementById('clearCompleted');
+const selectAllFilterButton = document.getElementById('selectAll');
+const selectCompletedFilterButton = document.getElementById('selectComplete');
+const selectActiveFilterButton = document.getElementById('selectActive')
 
-let toDoListData = [];
-let currentNewId = 0;
+let toDoList = [];
+let currentUniqueToDoId = 0;
 function getNewId() {
-    return currentNewId++;
+    return currentUniqueToDoId++;
 }
-function createToDoNode(toDoTitle, toDoId) {
+let currentFilter = 'all' // 'all', 'completed', 'active'
+function createToDoNode({id, title, isComplete}) {
     const newToDoContainer = document.createElement('li');
     newToDoContainer.className = 'toDoContainer';
-    newToDoContainer.id = toDoId
+    newToDoContainer.id = id
 
     const newToDoCompleteButton = document.createElement('button');
-    newToDoCompleteButton.innerHTML = '<span class="material-symbols-outlined">radio_button_unchecked</span>';
+    newToDoCompleteButton.innerHTML = `<span class="material-symbols-outlined">${isComplete?'radio_button_checked':'radio_button_unchecked'}</span>`;
     newToDoCompleteButton.className = 'toDoCompleteButton';
 
     const newToDoDeleteButton = document.createElement('button');
@@ -25,101 +29,210 @@ function createToDoNode(toDoTitle, toDoId) {
     newToDoDeleteButton.className = 'toDoDeleteButton';
 
     const newToDo = document.createElement('span');
-    newToDo.innerText = toDoTitle;
+    newToDo.innerText = title;
     newToDo.className = 'toDo';
+    if(isComplete) newToDo.classList.add('completeToDo');
 
-    newToDoCompleteButton.addEventListener('click', (event) => toggleToDo(event, toDoId, newToDo));
-    newToDoDeleteButton.addEventListener('click', (event) => removeToDo(event, toDoId, newToDoContainer));
+    newToDoCompleteButton.addEventListener('click', () => toggleToDo(id));
+    newToDoDeleteButton.addEventListener('click', () => removeToDo(id));
+    newToDo.addEventListener('dblclick', () => createRenameInput(id));
 
     newToDoContainer.appendChild(newToDoCompleteButton);
     newToDoContainer.appendChild(newToDo);
     newToDoContainer.appendChild(newToDoDeleteButton);
-    toDoList.appendChild(newToDoContainer);
+    toDoListContainer.appendChild(newToDoContainer);
 }
 
-function toggleToDo(event, id, toDo) {
-    const toDoData = toDoListData.find(toDo => toDo.id === id);
-    if(toDoData.isComplete){
-        toDoData.isComplete = false;
-        event.target.innerHTML = '<span class="material-symbols-outlined">radio_button_unchecked</span>';
-        toDo.classList.remove('completeToDo');
+function createRenameInput(toDoId) {
+    const input = document.createElement('input');
+    input.classList.add('renameToDoInput');
+    input.value = toDoList.find(toDo => toDo.id === toDoId).title;
+    const toDoContainer = document.getElementById(toDoId);
+    input.addEventListener('click', (event) => event.stopPropagation());
+    const clickHandler = (event) => {
+        if(event.target === input){
+            return;
+        }
+        event.stopPropagation();
+        removeInput();
     }
-    else {
-        toDoData.isComplete = true;
-        event.target.innerHTML = '<span class="material-symbols-outlined">radio_button_checked</span>';
-        toDo.classList.add('completeToDo');
+    const removeInput = () => {
+        if(!input.value.trim()) removeToDo(toDoId);
+        else {
+            toDoContainer.children[2].innerText = input.value;
+            toDoList.find(toDo => toDo.id === toDoId).title = input.value;
+        }
+        input.remove();
+        toDoContainer.children[1].classList.remove('noneToDo');
+        document.removeEventListener('click',  clickHandler, true);
     }
+    input.addEventListener('keydown', (event) => {
+        if(event.key === 'Enter' || event.key === 'Escape'){
+            if(!input.value.trim()) removeToDo(toDoId);
+            else{
+                toDoContainer.children[2].innerText = input.value;
+                toDoList.find(toDo => toDo.id === toDoId).title = input.value;
+                removeInput();
+            }
+        }
+    });
+    toDoContainer.children[1].classList.add('noneToDo');
+    toDoContainer.children[1].insertAdjacentElement('beforebegin', input);
+    document.addEventListener('click', clickHandler, true);
+    input.focus();
+}
+
+function toggleToDo(toDoId) {
+    const toDoData = toDoList.find(toDo => toDo.id === toDoId);
+    const toDoContainer = document.getElementById(toDoId);
+    if(toDoContainer){
+        if(currentFilter !== 'all') toDoContainer.remove();
+        else {
+            if(toDoData.isComplete){
+                toDoContainer.children[0].innerHTML = '<span class="material-symbols-outlined">radio_button_unchecked</span>';
+                toDoContainer.children[1].classList.remove('completeToDo');
+            }
+            else {
+                toDoContainer.children[0].innerHTML = '<span class="material-symbols-outlined">radio_button_checked</span>';
+                toDoContainer.children[1].classList.add('completeToDo');
+            }
+        }
+    }
+    toDoData.isComplete = !toDoData.isComplete;
     changeActiveToDoCount();
     checkDisplayClearButton();
 }
 
 function toggleAllToDo() {
-    const activeToDoIdList = toDoListData.filter((toDo) => !toDo.isComplete);
+    const activeToDoIdList = toDoList.filter((toDo) => !toDo.isComplete);
     if(activeToDoIdList.length){
-        activeToDoIdList.forEach(toDo => {
-            const toDoContainer = document.getElementById(toDo.id);
-            const allChildren = toDoContainer.children;
-            allChildren[0].innerHTML = '<span class="material-symbols-outlined">radio_button_checked</span>';
-            allChildren[1].classList.add('completeToDo');
-            toDo.isComplete = true;
-        });
+        switch (currentFilter) {
+            case 'all': {
+                activeToDoIdList.forEach(toDo => {
+                    toggleToDo(toDo.id);
+                });
+                break;
+            }
+            case 'completed': {
+                activeToDoIdList.forEach(toDo => {
+                    toDo.isComplete = true;
+                });
+                renderToDoList(toDoList);
+                break;
+            }
+            case 'active': {
+                activeToDoIdList.forEach(toDo => {
+                    toDo.isComplete = true;
+                });
+                toDoListContainer.innerHTML = '';
+                break;
+            }
+        }
     }
     else {
-        toDoListData.forEach(toDo => {
-            const toDoContainer = document.getElementById(toDo.id);
-            const allChildren = toDoContainer.children;
-            allChildren[0].innerHTML = '<span class="material-symbols-outlined">radio_button_unchecked</span>';
-            allChildren[1].classList.remove('completeToDo');
-            toDo.isComplete = false;
-        })
+        switch (currentFilter) {
+            case 'all': {
+                toDoList.forEach(toDo => {
+                    toggleToDo(toDo.id);
+                });
+                break;
+            }
+            case 'completed': {
+                toDoList.forEach(toDo => {
+                    toDo.isComplete = false;
+                });
+                toDoListContainer.innerHTML = '';
+                break;
+            }
+            case 'active': {
+                toDoList.forEach(toDo => {
+                    toDo.isComplete = false;
+                });
+                renderToDoList(toDoList);
+                break;
+            }
+        }
     }
     changeActiveToDoCount();
     checkDisplayClearButton();
 }
 
-function removeToDo(event, id, toDoContainer) {
-    toDoListData = toDoListData.filter((toDo) => toDo.id !== id);
-    toDoContainer.remove();
+function setFilter(filter){
+    if(currentFilter === filter) return;
+    switch (filter){
+        case 'all':{
+            if(currentFilter === 'active') selectActiveFilterButton.classList.remove('helperButtonActive')
+            else selectCompletedFilterButton.classList.remove('helperButtonActive');
+            selectAllFilterButton.classList.add('helperButtonActive');
+            renderToDoList(toDoList)
+            break;
+        }
+        case 'completed':{
+            if(currentFilter === 'active') selectActiveFilterButton.classList.remove('helperButtonActive')
+            else selectAllFilterButton.classList.remove('helperButtonActive');
+            selectCompletedFilterButton.classList.add('helperButtonActive');
+            renderToDoList(toDoList.filter(toDo => toDo.isComplete === true));
+            break;
+        }
+        case 'active':{
+            if(currentFilter === 'all') selectAllFilterButton.classList.remove('helperButtonActive')
+            else selectCompletedFilterButton.classList.remove('helperButtonActive');
+            selectActiveFilterButton.classList.add('helperButtonActive');
+            renderToDoList(toDoList.filter(toDo => toDo.isComplete === false))
+            break;
+        }
+    }
+    currentFilter = filter;
+}
+
+function renderToDoList(toDoList){
+    toDoListContainer.innerHTML = '';
+    toDoList.forEach(toDo => createToDoNode(toDo));
+}
+
+function removeToDo(toDoId) {
+    toDoList = toDoList.filter((toDo) => toDo.id !== toDoId);
+    const toDoContainer = document.getElementById(toDoId);
+    if(toDoContainer) toDoContainer.remove();
     checkDisplayHelper();
     changeActiveToDoCount();
     checkDisplayClearButton();
 }
+
 function addToDo(event) {
     event.preventDefault();
     if(!addToDoInput.value.trim()) return;
-
     const toDoTitle = addToDoInput.value.trim();
     const newToDoId = getNewId();
-    toDoListData.push({id: newToDoId, title: toDoTitle, isComplete: false});
-
-    createToDoNode(toDoTitle, newToDoId);
+    toDoList.push({id: newToDoId, title: toDoTitle, isComplete: false});
+    addToDoInput.value = '';
+    if(currentFilter !== 'completed') createToDoNode({id: newToDoId, title: toDoTitle, isComplete: false});
     checkDisplayHelper();
     changeActiveToDoCount();
-    addToDoInput.value = '';
 }
 function clearAllCompleteToDo() {
-    toDoListData.forEach(toDo => {
+    toDoList.forEach(toDo => {
         if(toDo.isComplete === true){
-            const toDoContainer = document.getElementById(toDo.id);
-            toDoContainer.remove();
+            removeToDo(toDo.id);
         }
     });
-    toDoListData = toDoListData.filter(toDo => !toDo.isComplete);
     checkDisplayClearButton();
     checkDisplayHelper();
 }
 function checkDisplayClearButton() {
-    clearCompletedToDo.className = toDoListData.find(toDo => toDo.isComplete === true)?'helperClearButton helperClearButtonActive':'helperClearButton';
+    clearCompletedToDo.className = toDoList.find(toDo => toDo.isComplete === true)?'helperClearButton helperClearButtonActive':'helperClearButton';
 }
 function checkDisplayHelper(){
-    helperContainer.className = toDoListData.length > 0?'toDoHelperContainer toDoHelperContainerActive':'toDoHelperContainer';
+    helperContainer.className = toDoList.length > 0?'toDoHelperContainer toDoHelperContainerActive':'toDoHelperContainer';
 }
 
 function changeActiveToDoCount() {
-    const activeToDoCount = toDoListData.reduce((acc, toDo) => !toDo.isComplete?++acc:acc, 0);
-    toDoCount.innerText = String(activeToDoCount);
+    toDoCount.innerText = String(toDoList.reduce((acc, toDo) => !toDo.isComplete?++acc:acc, 0));
 }
 
 clearCompletedToDo.addEventListener('click', clearAllCompleteToDo);
 addToDoForm.addEventListener('submit', addToDo);
 toggleAllToDoButton.addEventListener('click', toggleAllToDo);
+selectAllFilterButton.addEventListener('click', () => setFilter('all'));
+selectActiveFilterButton.addEventListener('click', () => setFilter('active'));
+selectCompletedFilterButton.addEventListener('click', () => setFilter('completed'));
